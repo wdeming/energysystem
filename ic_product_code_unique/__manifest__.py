ecda185411900961c910f79b3ebbd142305ee034
wdeming <57711720+wdeming@users.noreply.github.com>
2020-07-05 20:27:39 +0500

    Create __manifest__.py

---
+++ ic_product_code_unique/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Product Code Unique",
+    'license':  "Other proprietary",
+    'summary': "Makes the product code unique like the barcode",
+    'author': "IC",
+    'website': "www.ichat.com",
+    'category': 'Warehouse',
+    'version': '13',
+    'depends': ['base','product'],
+    'data': [
+              ],
+    'images':  ["static/description/ic_log.png"],
+    'auto_install': False,
+}
