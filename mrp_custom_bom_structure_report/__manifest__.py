35c851515763d9d39801d56a700f51a448a83586
wdeming <57711720+wdeming@users.noreply.github.com>
2020-10-05 10:24:02 +0500

    Create __manifest__.py

---
+++ mrp_custom_bom_structure_report/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "MRP: Custom BoM structure report",
+
+    'summary': "MRP: Custom BoM structure report",
+
+    'description': "MRP: Custom BoM structure report",
+
+    'author': "Colibri",
+    'website': "http://clbr.uz",
+    'category': 'MRP',
+    'version': '0.1',
+    
+    "application": True,
+    "installable": True,
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'mrp'],
+}
