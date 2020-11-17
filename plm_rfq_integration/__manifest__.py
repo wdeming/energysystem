d849b7c3e0f860c62b24a238cd909e86beb6d296
wdeming <57711720+wdeming@users.noreply.github.com>
2020-11-17 09:05:52 +0500

    Create __manifest__.py

---
+++ plm_rfq_integration/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'PLM & RFQ integration',
+    'version': '0.2',
+    'category': 'Sales',
+    'summary': 'PLM & RFQ integration',
+
+    'author': 'Colibri',
+    'website': 'https://clbr.uz',
+    'depends': [
+        'mrp_plm',
+        'purchase',
+        'sale'
+        # 'studio_customization'
+    ],
+    'data': [
+        # 'views/mrp_plm_views.xml',
+    ],
+    'application': False,
+}
